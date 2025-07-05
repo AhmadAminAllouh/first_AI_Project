919e56042ba6c149fa133e73970347c036b5861e
Dr.dom <ahmamdamin17@gmail.com>
2025-07-05 17:38:06 +0300

    first_step

---
+++ project one.py
@@ -0,0 +1,7 @@
+print("hello world ")
+num = eval(input("enter a number"))
+number = eval(input("enter a second number"))
+if num > number:
+    print("number is greater")
+else:
+    print("number", num, "is not greater than", number)
